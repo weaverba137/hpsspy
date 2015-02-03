8785464e2483d7a9a3476cd4f262b29bf892c698
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2015-02-03 00:16:16 -0500

    Moving code into module files.

---
+++ hpsspy/scan/__init__.py
@@ -0,0 +1,12 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""
+===========
+hpsspy.scan
+===========
+
+Functions for scanning directory trees to find files in need of backup.
+"""
+from .compile_map import compile_map
+from .files_to_hpss import files_to_hpss
+from .scan_hpss import scan_hpss
---
+++ hpsspy/scan/files_to_hpss.py
@@ -0,0 +1,32 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+def files_to_hpss(hpss_map_cache):
+    """Create a map of files on disk to HPSS files.
+
+    Parameters
+    ----------
+    hpss_map_cache : str
+        Data file containing the map.
+
+    Returns
+    -------
+    files_to_hpss : dict
+        The mapping.
+    """
+    import logging
+    from os.path import exists
+    from . import compile_map
+    logger = logging.getLogger(__name__)
+    if exists(hpss_map_cache):
+        logger.info("Found map file {0}.".format(hpss_map_cache))
+        with open(hpss_map_cache) as t:
+            hpss_map = json.load(t)
+    else:
+        hpss_map = {
+            "dr8":{"exclude":[],"casload":{},"apogee":{},"boss":{},"sdss":{}},
+            "dr9":{"exclude":[],"casload":{},"apogee":{},"boss":{},"sdss":{}},
+            "dr10":{"exclude":[],"casload":{},"apogee":{},"boss":{},"sdss":{}},
+            "dr11":{"exclude":[],"casload":{},"apogee":{},"boss":{},"marvels":{},"sdss":{}},
+            "dr12":{"exclude":[],"casload":{},"apo":{},"apogee":{},"boss":{},"marvels":{},"sdss":{}},
+            }
+    hpss_map = compile_map(hpss_map,options.release)
