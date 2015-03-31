31c951a61568981446b5a313b3a386ee308c0b6f
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2015-03-31 15:07:43 -0400

    more processing

--- hpsspy/scan/process_missing.py
+++ hpsspy/scan/process_missing.py
@@ -22,21 +22,28 @@ def process_missing(missing_cache,disk_root,hpss_root,dirmode='2770'):
     import json
     from os import chdir
     from os.path import basename, dirname, join
-    from ..util import hsi
+    from ..util import hsi, htar
     logger = logging.getLogger(__name__)
     logger.debug("Processing missing files from {0}.".format(missing_cache))
     with open(missing_cache) as fp:
         missing = json.load(fp)
+    created_directories = set()
     for h in missing:
         if h.endswith('.tar'):
             if h.endswith('_files.tar'):
                 htar_dir = '-L files'
-                chdir = dirname(h)
+                disk_chdir = dirname(h)
             else:
                 htar_dir = basename(h).split('_')[-1].split('.')[0]
-                chdir = dirname(h)
-            command = "chdir({0}); hsi('mkdir','-p','-m','{1}','{2}'); htar -cvf {3} {4}".format(join(disk_root,chdir), dirmode, join(hpss_root,chdir), join(hpss_root,h), htar_dir)
+                disk_chdir = dirname(h)
+            logger.debug("chdir('{0}')".format(join(disk_root,disk_chdir)))
+            # chdir(join(disk_root,disk_chdir))
+            h_dir = join(hpss_root,disk_chdir)
+            if h_dir not in created_directories:
+                logger.debug("hsi('mkdir', '-p', '-m', '{0}', '{1}')".format(dirmode,h_dir))
+                # out = hsi('mkdir', '-p', '-m', dirmode, h_dir)
+                created_directories.add(h_dir)
+            logger.debug("htar('-cvf', '{0}', '-H', 'crc:verify=all', '{1}')".format(hpss_root,h), htar_dir)
         else:
-            command = "hsi('put', '{0}', ':', '{1}')".format(join(disk_root,missing[h][0]),join(hpss_root,h))
-        logger.debug(command)
+            logger.debug("hsi('put', '{0}', ':', '{1}')".format(join(disk_root,missing[h][0]),join(hpss_root,h)))
     return
