c5455764782d99205e81ffc1cb297719ffa1fbea
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2022-05-20 12:11:31 -0700

    not using tox after all

--- hpsspy/__init__.py
+++ hpsspy/__init__.py
@@ -6,7 +6,7 @@ hpsspy
 
 Python interface to the HPSS system.
 """
-from ._version import version as __version__
+__version__ = '1.0.0.dev316'
 
 
 class HpssError(Exception):
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 
 import os
 import sys
-
+from importlib import import_module
 from setuptools import setup
 
 # from extension_helpers import get_extensions
@@ -19,21 +19,12 @@ TEST_HELP = """
 Note: running tests is no longer done using 'python setup.py test'. Instead
 you will need to run:
 
-    tox -e test
-
-If you don't already have tox installed, you can install it with:
-
-    pip install tox
-
-If you only want to run part of the test suite, you can also use pytest
-directly with::
-
-    pip install -e .[test]
     pytest
 
-For more information, see:
+If you don't already have pytest installed, you can install it with:
 
-  http://docs.astropy.org/en/latest/development/testguide.html#running-tests
+    pip install pytest
+
 """
 
 if 'test' in sys.argv:
@@ -44,38 +35,17 @@ DOCS_HELP = """
 Note: building the documentation is no longer done using
 'python setup.py build_docs'. Instead you will need to run:
 
-    tox -e build_docs
+    sphinx-build -W --keep-going -b html doc doc/_build/html
 
-If you don't already have tox installed, you can install it with:
+If you don't already have Sphinx installed, you can install it with:
 
-    pip install tox
+    pip install Sphinx
 
-You can also build the documentation with Sphinx directly using::
-
-    pip install -e .[docs]
-    cd docs
-    make html
-
-For more information, see:
-
-  http://docs.astropy.org/en/latest/install.html#builddocs
 """
 
 if 'build_docs' in sys.argv or 'build_sphinx' in sys.argv:
     print(DOCS_HELP)
     sys.exit(1)
 
-VERSION_TEMPLATE = """
-# Note that we need to fall back to the hard-coded version if either
-# setuptools_scm can't be imported or setuptools_scm can't determine the
-# version, so we catch the generic 'Exception'.
-try:
-    from setuptools_scm import get_version
-    version = get_version(root='..', relative_to=__file__)
-except Exception:
-    version = '{version}'
-""".lstrip()
-
-setup(use_scm_version={"write_to": os.path.join("hpsspy", "_version.py"),
-                       "write_to_template": VERSION_TEMPLATE})
-      # ext_modules=get_extensions())
+module = import_module('hpsspy')
+setup(version=module.__version__)
